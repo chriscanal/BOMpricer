489b1d169f333217e73e7843630cd4186708a2a7
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2015-04-16 20:27:53 -0400

    Got dis stuff working

--- BOMpricer.py
+++ BOMpricer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import requests
 import json
 from pprint import pprint
@@ -10,23 +11,25 @@ API_Key = '8e2b4c56-05aa-4ce9-82dc-be9a660bb1ea'
 endpoint = 'http://inventory.api.eciaauthorized.com/api/Search/Query'
 
 query_params =  {
-                    "CompanyID": "iRobot",
-                    "APIKey": "8e2b4c56-05aa-4ce9-82dc-be9a660bb1ea",
-                    "CountryCode": "",
-                    "CurrencyCode": "",
-                    "InStockOnly": "false",
-                    "ExactMatch": "false",
-                    "Queries": [
-                        {
-                            "SearchToken": "rf201"
-                        }
-                    ]
-                }
-
-
-response = requests.get(endpoint, params = query_params)
-print "The request.get just ran"
+    "CompanyID": "iRobot",
+    "APIKey": "8e2b4c56-05aa-4ce9-82dc-be9a660bb1ea",
+    "CountryCode": "",
+    "CurrencyCode": "",
+    "InStockOnly": "false",
+    "ExactMatch": "false",
+    "Queries": [
+        {
+            "SearchToken": "rf202"
+        }
+    ]
+}
+oral =  { 'Content-Type': 'application/json'}
+#print query_params
+#print json.dumps(query_params)
+response = requests.post(endpoint, data = json.dumps(query_params), headers= oral)
+#print vars(response)
+#print "The request.get just ran"
 data = response.json
-print "The response.json just ran"
+#print "The response.json just ran"
 pprint.pprint(data)
-print "The pprint just ran"
+#print "The pprint just ran"
